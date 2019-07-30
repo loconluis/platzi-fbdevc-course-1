d7981f178b9cb03cbee0d52bd2c727d253596368
Luis Locon <luis.locon@gmail.com>
2019-07-29 18:49:06 -0600

    [New components and structure of the site]

---
+++ src/containers/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Main from '../components/Main'
+import Sidebar from '../components/Sidebar'
+import Info from '../components/Info'
+import About from '../components/About'
+import Education from '../components/Education'
+import Experience from '../components/Experience'
+import Certificates from '../components/Certificates'
+import Skills from '../components/Skills'
+
+export default function App() {
+  return (
+    <Main>
+      <Sidebar>
+        <About />
+      </Sidebar>
+      <Info>
+        <Education />
+        <Experience />
+        <Certificates />
+        <Skills />
+      </Info>
+    </Main>
+  )
+}
