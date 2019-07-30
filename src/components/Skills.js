fa45079afc7f26daf49d1d9149cbecc55dcf260d
Luis Locon <luis.locon@gmail.com>
2019-07-29 19:11:40 -0600

    [Skills structure]

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,6 +4,13 @@ export default function Skills() {
   return (
     <div className="Skills">
       <div className="Skills-container">
+        <div className="Skills-item">
+          <h5>React</h5>
+          <div className="Skill-line">
+            <span>100</span>
+          </div>
+          <p>Good with react</p>
+        </div>
       </div>
     </div>
   )
